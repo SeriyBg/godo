a91272469889554990540df98ed57ccfadea0ce3
Serge Bishyr <seriybg@gmail.com>
2017-10-03 11:05:05 +0300

    add json marshaling/unmarshaling tests for domain

---
+++ repository/domain_test.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNote_MarshalJSON(t *testing.T) {
+	timeNow := time.Date(2017, 11, 3, 1, 12, 42, 1, time.UTC)
+	note := &Note{
+		id:          "any_gen_id",
+		name:        "note_name",
+		description: "note_desc",
+		state:       New,
+		created:     timeNow,
+		updated:     timeNow,
+	}
+	expectedJson := "{\"id\":\"any_gen_id\",\"name\":\"note_name\"," +
+		"\"description\":\"note_desc\"," +
+		"\"state\":\"New\"," +
+		"\"created\":\"2017-11-03T01:12:42.000000001Z\"," +
+		"\"updated\":\"2017-11-03T01:12:42.000000001Z\"}"
+	bytes, err := json.Marshal(note)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedJson, string(bytes))
+}
+
+func TestNote_UnmarshalJSON(t *testing.T) {
+	jsonNote := "{\"id\":\"any_gen_id\",\"name\":\"note_name\"," +
+		"\"description\":\"note_desc\"," +
+		"\"state\":\"New\"," +
+		"\"created\":\"2017-11-03T01:12:42.000000001Z\"," +
+		"\"updated\":\"2017-11-03T01:12:42.000000001Z\"}"
+
+	note := &Note{}
+	err := json.Unmarshal([]byte(jsonNote), note)
+	assert.NoError(t, err)
+
+	timeNow := time.Date(2017, 11, 3, 1, 12, 42, 1, time.UTC)
+	expectedNote := Note{
+		id:          "any_gen_id",
+		name:        "note_name",
+		description: "note_desc",
+		state:       New,
+		created:     timeNow,
+		updated:     timeNow,
+	}
+	assert.Equal(t, expectedNote, *note)
+}
